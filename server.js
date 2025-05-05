205de00e78ff704f2b75520e26b44332a0e9be2a
nihatsaydam <nihat.saydam@icloud.com>
2025-05-05 05:27:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -413,7 +413,7 @@ Mesaj: ${message}`
     roomNumber:  { type: String, required: true },
     username:    { type: String, required: true, default: 'Unknown' },
     message:     { type: String, required: true },
-    status:      { type: String, enum: ['waiting','active','resolved'], default: 'waiting' },
+    status:      { type: String, enum: ['waiting','active','completed'], default: 'waiting' },
     sender:      { type: String, enum: ['user','bot'], required: true },
     timestamp:   { type: Date, default: Date.now }
   }, { collection: 'Concierge' });
